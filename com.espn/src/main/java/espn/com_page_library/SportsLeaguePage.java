44c7da41949e86307e756c841a76bc00a47b767c
adilkhaleque <adilkhaleque429@gmail.com>
2022-11-08 18:01:36 -0500

    sports league

--- com.espn/src/main/java/espn/com_page_library/SportsLeaguePage.java
+++ com.espn/src/main/java/espn/com_page_library/SportsLeaguePage.java
@@ -1,6 +1,32 @@
 package espn.com_page_library;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import shared.SharedStepsUI;
 
 public class SportsLeaguePage extends SharedStepsUI {
+    @FindBy (xpath = "//ul[@class='first-group']//li[@class='sub']/a[@href and @name='&lpos=subnav+subnav_f1_drivers']")
+    public WebElement f1DriverButton;
+
+    @FindBy (xpath = "//tbody//tr[10]//a[@href]")
+    public WebElement lewisHamiltonLink;
+
+    public SportsLeaguePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickOnDriversButton() {
+        safeClickOnElement(f1DriverButton);
+    }
+
+    public AthletePage clickOnLewisHamilton() {
+        jsScrollDownUntilElementIsVisible(lewisHamiltonLink);
+        safeClickOnElement(lewisHamiltonLink);
+
+        return new AthletePage();
+    }
+
+
+
 }
--- com.espn/src/main/java/espn/com_page_library/TeamPage.java
+++ com.espn/src/main/java/espn/com_page_library/TeamPage.java
@@ -1,15 +1,26 @@
 package espn.com_page_library;
 
-import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import shared.SharedStepsUI;
 
 public class TeamPage extends SharedStepsUI {
 
+    @FindBy (xpath = "//ul[@class='Nav__Secondary__Menu center flex items-center relative']//li[4]/a")
+    public WebElement lakersScheduleButton;
+
+    @FindBy (xpath = "//div[@class='flex justify-between mt3 mb3 items-center']/h1")
+    public WebElement lakersScheduleTitle;
 
     public TeamPage() {
         PageFactory.initElements(driver, this);
     }
+
+    public String getLakersScheduleTitleText() {
+        return getTrimmedElementText(lakersScheduleTitle);
+    }
+
 
 
 }
